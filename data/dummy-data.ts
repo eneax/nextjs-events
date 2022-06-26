076273d2fb18d8c5d29adbbe9b25d644cb595e11
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-06-26 18:09:31 +0200

    Style Event Details Page

--- data/dummy-data.ts
+++ data/dummy-data.ts
@@ -62,6 +62,6 @@ export function getFilteredEvents(dateFilter) {
   return filteredEvents;
 }
 
-export function getEventById(id) {
+export function getEventById(id: string): Event | undefined {
   return DUMMY_EVENTS.find((event) => event.id === id);
 }
--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,10 +1,60 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+import { getEventById } from "data/dummy-data";
+import { FiCalendar, FiHome } from "react-icons/fi";
+import Image from "next/image";
 
 const EventDetailsPage: NextPage = () => {
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId as string);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  const formattedDate = new Date(event.date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Event Details Page</h1>
-    </div>
+    <section className="max-w-screen-md px-4 py-16 mx-auto text-center lg:py-32">
+      <h1 className="text-4xl font-bold sm:text-6xl">{event.title}</h1>
+
+      <div className="block mt-4">
+        <div className="mx-auto w-full h-72 relative">
+          <Image
+            src={`/${event.image}`}
+            alt={event.title}
+            layout="fill"
+            placeholder="blur"
+            blurDataURL={`/${event.image}`}
+            className="object-cover rounded-bl-3xl rounded-tr-3xl"
+          />
+        </div>
+
+        <div className="flex items-center justify-center flex-col md:flex-row mt-4 space-x-4">
+          <time className="font-medium flex items-center mb-1">
+            <FiCalendar className="inline-block mr-1" />
+            {formattedDate}
+          </time>
+
+          <span className="w-8 h-px bg-yellow-500 hidden md:block"></span>
+
+          <address className="opacity-50 flex items-center mb-1">
+            <FiHome className="inline-block mr-1" />
+            {event.location}
+          </address>
+        </div>
+      </div>
+
+      <p className="max-w-lg mx-auto mt-4 text-sm text-gray-500">
+        {event.description}
+      </p>
+    </section>
   );
 };
 
