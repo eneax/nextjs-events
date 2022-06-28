83c83fe39b4e369e79bd7a3db8147668195740ce
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-06-28 10:00:47 +0200

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from "next";
+import Empty from "components/Empty";
+
+const NotFound: NextPage = () => <Empty header="404" text="Page not found." />;
+
+export default NotFound;
