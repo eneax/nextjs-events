a49448e0437bf2e2033e503835b0dadfd80b4ca0
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-07-07 17:19:52 +0200

    Add a loading state when fetching comments

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -14,7 +14,7 @@ const Loading = ({ text = "Loading", speed = 300 }) => {
   }, [text, speed]);
 
   return (
-    <p className="absolute left-0 right-0 mt-8 text-center text-2xl font-bold sm:text-3xl">
+    <p className="absolute left-0 right-0 mt-8 text-center text-xl font-bold">
       {content}
     </p>
   );
