6ea27f824f0be8c6b7584038477081ad5965a7ec
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-07-08 14:18:01 +0200

    Add footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,6 +19,22 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
       <Header />
       <main>{children}</main>
+
+      <footer className="bg-gray-100">
+        <div className="max-w-screen-xl px-4 pb-8 mx-auto sm:px-6 lg:px-8">
+          <div className="pt-8 border-t border-gray/10">
+            <p className="text-xs leading-relaxed text-center text-gray-600">
+              Code available on{" "}
+              <a
+                href="https://github.com/eneax/nextjs-events"
+                className="underline transition hover:text-gray-900"
+              >
+                GitHub
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
     </React.Fragment>
   );
 };
