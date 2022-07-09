054b9ab8e83e8077cbf5592f2c1a5ff2c5361e71
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-07-09 18:07:05 +0200

    Create reusable Header component

---
+++ components/Nav.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const Navbar = () => (
+  <nav className="flex items-center justify-between max-w-3xl p-4 mx-auto">
+    <Link href="/">
+      <a className="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg">
+        🎭
+      </a>
+    </Link>
+
+    <ul className="flex items-center space-x-2 text-sm font-medium text-gray-500">
+      <li>
+        <Link href="/events">
+          <a className="px-3 py-2 rounded-lg">All Events</a>
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
